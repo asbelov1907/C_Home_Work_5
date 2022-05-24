1ab3d4a3f8ba3e519f145f36d6d64e33a1584bde
AntonBelov <asbelov1907@gmail.com>
2022-05-24 14:35:34 +0300

    Задача 1 выполнена

---
+++ Task_1/Program.cs
@@ -0,0 +1,28 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+
+void EvenNumbers(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("Четные числа: ");
+     for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            Console.Write(arr[i] + " ");
+    }
+}
+
+
+int[] array = new int[4];
+    Random randomaiser = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randomaiser.Next(100,1000);
+}
+
+EvenNumbers(array);
