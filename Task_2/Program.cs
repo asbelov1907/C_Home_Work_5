c52b090d98a601d24a2529589d64d2aec03e61bf
AntonBelov <asbelov1907@gmail.com>
2022-05-24 14:43:45 +0300

    Задача 2 выполнена

---
+++ Task_2/Program.cs
@@ -0,0 +1,28 @@
+﻿//  Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+void UnevenNumbers(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+    Console.Write("Не четные числа: ");
+    int Uneven = 0;
+     for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 != 0)
+            Uneven += arr[i];
+            
+    }
+    Console.Write(Uneven + " ");
+}
+
+
+int[] array = new int[4];
+    Random randomaiser = new Random();
+
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = randomaiser.Next(-1000,1000);
+}
+
+UnevenNumbers(array);
